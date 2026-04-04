ec5404a74fd4c62662e08842c673c55f6fe98283
saurabhgupta01 <saurabh.gupta@prgx.com>
2026-04-04 17:15:50 +0530

    wip

--- src/main/java/com/nagp/search/config/CorsConfig.java
+++ src/main/java/com/nagp/search/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200","http://a971844839b9149958f0377a95a08889-89800353.ap-south-1.elb.amazonaws.com","*")
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                         .allowedHeaders("*")
